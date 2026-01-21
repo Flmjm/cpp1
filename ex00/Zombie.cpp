c0f66c920f69068c26054f692ae1341b394b2acd
manu <manual2013@hotmail.fr>
2026-01-21 17:09:29 +0100

    aled

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,17 @@
+#include <Zombie.hpp>
+
+void Zombie::announce()
+{
+	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::Zombie(std::string name)
+{
+	_name = name;
+	announce();
+}
+
+Zombie::~Zombie()
+{
+	std::cout << _name << " destruct" << std::endl;
+}
