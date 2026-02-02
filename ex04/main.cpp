1c70281ee0be00c902091b6fb10b86402fc58f3a
manu <manual2013@hotmail.fr>
2026-02-02 20:11:25 +0100

    finish

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 int main(int argc,  char**argv)
@@ -9,15 +10,42 @@ int main(int argc,  char**argv)
 		return 1;
 	}
 
-	std::string filename(argv[1]), needle(argv[2]), haystack(argv[3]);
+	std::string filename(argv[1]), needle(argv[2]), replace(argv[3]);
+	std::string lineRead;
 
-	int tmp = filename.find(needle);
-	
+	std::ifstream fileOpen(filename);
+	if (!fileOpen)
+	{
+		std::cout << "Error, can't open " << filename << std::endl;
+		return 1;
+	}
 
-	// while (filename.find(needle))
-	// {
+	std::ofstream fileReplace(filename + ".replace");
+	if (!fileReplace)
+	{
+		std::cout << "Error, can't open " << filename + ".replace" << std::endl;
+		return 1;
+	}
 
-	// }
+	while (getline(fileOpen, lineRead))
+	{
+		int i = 0;
+
+
+		i = lineRead.find(needle);
+		while (i != -1)
+		{
+			lineRead.erase(i , needle.length());
+			lineRead.insert(i , replace);
+			i = lineRead.find(needle);
+		}
+
+
+		fileReplace << lineRead << std::endl;
+	}
+
+	fileReplace.close();
+	fileOpen.close();
 
 	return 0;
 }
---
+++ ex05/main.cpp
@@ -0,0 +1,10 @@
+#include "Harl.hpp"
+
+int main()
+{
+	Harl aled;
+	aled.complain("DEBUG");
+	aled.complain("INFO");
+
+	return 0;
+}
